959c9d5661d3954a99b3c4cac01a1806a7db2cf1
mming <szmingleo@gmail.com>
2020-03-27 17:53:28 +0800

    https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash

--- diy.sh
+++ diy.sh
@@ -9,4 +9,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/vernesong/OpenClash/luci-app-openclash package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-openclash
